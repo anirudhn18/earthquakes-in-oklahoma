5b55f6cf465b648e27867fcaf4eb1193db3c9ca8
Anirudh Narayanan <anirudhn18@yahoo.com>
2016-08-04 05:53:54 -0500

    changes to oil extraction script and renaming some files

--- Codes/01-oil-data-extraction.py
+++ Codes/01-oil-data-extraction.py
@@ -1,32 +1,34 @@
 ##Data extraction module for oil well raw data
 ##Set working directory to where you have this code before execution
+#%reset -f
 
-import zipfile,os
+import zipfile as zf ,os
 import pandas as pd
 
-
+#Location of raw data
 ziploc = '..\\Files\\oil-wells-raw-data'
-zipfilelist = os.listdir(ziploc)
+list_of_zipfiles = pd.Series(os.listdir(ziploc))
+list_of_zipfiles = list_of_zipfiles[list_of_zipfiles.str.contains('.zip|.rar')]
 
-print 'zip file list'
-print zipfilelist
 
-zf = zipfile.ZipFile( '{}\\{}'.format(ziploc,zipfilelist[0])   ,'r')
+for a_file in list_of_zipfiles:
+    a_zipfile = zf.ZipFile("{}\\{}".format(ziploc,a_file),'r')
+    for content_file in a_zipfile.namelist():
+        with a_zipfile.open(content_file) as f:
+            try:
+                if 'Summary' in content_file:
+                    prod_summ.append(pd.read_csv(f),ignore_index = False)
+                else:
+                    if 'Head' in content_file:
+                        well_headers.append(pd.read_csv(f),ignore_index = False)
+                    else:
+                        prod.append(pd.read_csv(f),ignore_index = False)
 
-print 'sample zip file contents'
-print zf.namelist()
-
-for filename in zf.namelist():
-    with zf.open(filename) as f:
-        if 'Summary' in filename:
-            prod_summ = pd.read_csv(f)
-        else:
-            if 'Head' in filename:
-                well_headers = pd.read_csv(f)
-            else:
-                prod = pd.read_csv(f)
-
-print 'output data snaps'
-print prod_summ.head()
-print well_headers.head()
-print prod.head()
+            except:
+                if 'Summary' in content_file:
+                    prod_summ = pd.read_csv(f)
+                else:
+                    if 'Head' in content_file:
+                        well_headers = pd.read_csv(f)
+                    else:
+                        prod = pd.read_csv(f)
